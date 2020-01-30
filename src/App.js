9512ab0773a42e2d9f33e32e1554b5dafa66086a
JoniMujica <jonimujica3@hotmail.com>
2020-01-30 13:33:06 -0300

    creo formulario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
+class Formulario extends React.Component{
+  
+  state = {
+    nom: null,
+    ap: null,
+    mail:null,
+    tel:null,
+  };
+
+  handleChange = (evento) => {
+    const inputNombre = document.querySelector("#nombre");
+    this.setState({ nom: inputNombre.value });
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Inicio</h1>
+          <ul>
+            {this.state.nom}
+          </ul>
+        <form className="listado">
+          <ul>
+            <li>
+              <label>Nombre</label>
+              <input onInput={this.props.nom} className="estilos" id="nombre"></input>
+            </li>
+            <li>
+              <label>Apellido</label>
+              <input onInput={this.props.ap} className="estilos"></input>
+            </li>
+            <li>
+              <label>Email</label>
+              <input onInput={this.props.mail} className="estilos"></input>
+            </li>
+            <li>
+              <label>telefono</label>
+              <input onInput={this.props.tel} className="estilos"></input>
+            </li>
+          </ul>
+          <button className="eboton">Imprimir</button>
+        </form>
+      </div>
+  )
+  }
+}
+
+
 function App() {
+  let datos = {
+    nombre: null,
+    apellido: null,
+    email:null,
+    telefono:null,
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Formulario data={datos} />
       </header>
     </div>
   );
